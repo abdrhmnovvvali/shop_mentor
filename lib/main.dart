beea4aa3dcf875e68149fea08f3ba23f69e44884
Gulara Gulmammadova <gularagulmammadova@gmail.com>
2025-04-07 02:34:44 +0400

    - create splash_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_mentor/ui/app.dart';
+import 'package:shop_mentor/ui/app.dart'; 
 
 void main() {
   runApp(const MyApp());
--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashPage(),
+      home: const SplashPage(),
     );
   }
 }
